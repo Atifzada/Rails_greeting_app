d806762391cbc8a3954399760837e4202517f600
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-21 09:38:10 +0500

    Setup api as per the requirments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+greetings = [
+  "Welcome to the start of something amazing!",
+  "Hey there, ready to dive into new horizons?",
+  "Greetings and salutations!",
+  "Hello, curious mind! Let's explore together.",
+  "Step into our world with a warm hello!",
+  "Hola! Your journey with us begins now.",
+  "A friendly hello to kick off your day!",
+  "Hello, fellow explorer of the unknown!",
+  "Greetings, wayfarer of the digital realm!",
+  "Welcome to our realm of innovation and ideas!"
+]
+
+
+greetings.each do |content|
+  Message.create(content: content)
+end
